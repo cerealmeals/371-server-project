34cb783c4830eb0f7bff1bd3c80a20389478a7a6
sam <samnygard@gmail.com>
2024-10-16 12:23:07 -0700

    UDP tests work

--- tests/UDPclient.py
+++ tests/UDPclient.py
@@ -1,7 +1,14 @@
 from socket import *
 
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
+hostname = gethostname()
+IPAddr = gethostbyname(hostname)
 
 serverName = IPAddr
 serverPort = 1200
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = ('Input lowercase sentence: what a great upcase sentence')
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+clientSocket.close()
