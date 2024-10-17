cb69e653284c9e85476797d1a67e498947584a57
sam <samnygard@gmail.com>
2024-10-17 09:55:06 -0700

    more HTTP testing

--- SingleThreaded/TCPserver.py
+++ SingleThreaded/TCPserver.py
@@ -10,10 +10,32 @@ serverSocket.bind((hostname,serverPort))
 serverSocket.listen(1)
 
 print ('The server is ready to receive')
-
-while True:
+flag = True
+while flag:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    # multi threaded you make a new thread to do everything below this
+    response = connectionSocket.recv(2048).decode()
+
+    print('Whole response:\n',response, '\nEND OF WHOLE RESPONSE')
+    lines_in_response = response.split('\n')
+    words_in_request_line = lines_in_response[0].split(' ')
+    command = words_in_request_line[0]
+    version = words_in_request_line[2]
+
+    last_in_list = len(words_in_request_line)-1
+
+    R_char_and_L_char = words_in_request_line[last_in_list][-2:]
+    print(R_char_and_L_char)
+    if(R_char_and_L_char == '\n'):
+        print('yassss')
+    match command:
+        case 'GET':
+            print('http version:', version)
+            to_send = version + ' 200 OK'
+            connectionSocket.send(to_send.encode())
+            flag = False
+        case 'HEAD':
+            connectionSocket.send('Not Implemented'.encode())
+        case _: # Should be bad request because not a normal command
+            connectionSocket.send('Bad request'.encode())
     connectionSocket.close()
---
+++ runtests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+host=$(hostname)
+port=${1}
+# telnet $host $port << EOF
+# GET url HTTP/1.1
+# EOF
+# sleep 1
+
+echo "open ${host} ${port}" 
+sleep 1
+echo "GET /test.html HTTP/1.1" 
+echo "Host: sfu.ca"
+echo
+sleep 2
