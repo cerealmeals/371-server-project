ac80804891fa8a8db3771126f3b0645d6b576e34
Sam <samnygard@gmail.com>
2024-10-22 20:01:21 -0700

    made some test files for the proxy server and made the full multithreaded file

---
+++ proxytests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 
+host=${1}
+port=${2}
+url=${3}
+proxy=${4}
+proxyPort=${5}
+
+# Test Not found 404
+curl -i -o -x "${proxy}:${proxyPort} output404.txt "${host}:${port}/badurl"
+# Test Not Modified 304
+curl -i -o -x "${proxy}:${proxyPort} output304.txt --header 'If-Modified-Since: Sat, 20 Oct 2024 01:24:29 GMT' "${host}:${port}/${url}"
+# Test Not Implemented 501
+curl -i -o -x "${proxy}:${proxyPort} output501.txt -I "${host}:${port}/${url}"
+# Test Bad resquest 400
+curl -i -o -x "${proxy}:${proxyPort} output400.txt -X WRONG "${host}:${port}/${url}"
+# Test OK 200 and when "if modified" is later than "last modified"
+curl -i -o -x "${proxy}:${proxyPort} output200.txt --header 'If-Modified-Since: Sat, 18 Oct 2024 01:24:29 GMT' "${host}:${port}/${url}"
