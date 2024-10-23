d1f4b40467bad9b2a98db8e6ffb8a2b3c431b6d0
sam <samnygard@gmail.com>
2024-10-22 21:34:13 -0700

    slight edits to the bash scripts

--- proxytests.sh
+++ proxytests.sh
@@ -12,7 +12,7 @@ curl -i -x "${proxy}:${proxyPort}" -o proxyoutput404.txt "${host}:${port}/badurl
 # Test Not Modified 304
 curl -i -x "${proxy}:${proxyPort}" -o proxyoutput304.txt --header 'If-Modified-Since: Sat, 20 Oct 2024 01:24:29 GMT' "${host}:${port}/${url}"
 # Test Not Implemented 501
-curl -i -x "${proxy}:${proxyPort}" -o proxyoutput501.txt -I "${host}:${port}/${url}"
+curl -i -x "${proxy}:${proxyPort}" -o proxyoutput501.txt -X POST "${host}:${port}/${url}"
 # Test Bad resquest 400
 curl -i -x "${proxy}:${proxyPort}" -o proxyoutput400.txt -X WRONG "${host}:${port}/${url}"
 # Test OK 200 and when "if modified" is later than "last modified"
