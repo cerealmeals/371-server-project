c951b59e6d222d36d13b83610e0086409d7b542d
Sam <samnygard@gmail.com>
2024-10-23 18:38:56 -0700

    toHandInFolder

---
+++ toHanbIn/TCPSever.py
@@ -0,0 +1,81 @@
+from socket import *
+from _thread import *
+import threading
+from HTTPfunctions import *
+
+# create TCP connection
+hostname = gethostname()
+IPAddr = gethostbyname(hostname)
+
+serverPort = 8088
+serverSocket = socket(AF_INET,SOCK_STREAM)
+
+serverSocket.bind((hostname,serverPort))
+serverSocket.listen(5)
+
+print ('The server is ready to receive at ID:', IPAddr, 'and Port:', serverPort)
+
+def mutil_thread(connectionSocket):
+    # while True:
+        
+        response = connectionSocket.recv(2048).decode()
+        # if len(response) <= 0:
+        #     print('no data')
+        #     break
+
+
+        # split the response line by line
+        lines_in_response = response.split('\n')
+
+        for i in range(len(lines_in_response)):
+            print(lines_in_response[i])
+            lines_in_response[i] = lines_in_response[i][:-1]
+        
+
+        # get the first line and decode
+        words_in_request_line = lines_in_response[0].split(' ')
+        command = words_in_request_line[0]
+        url = words_in_request_line[1]
+        version = words_in_request_line[2]
+        
+
+        print("RESPONSE:")
+        match command:
+            case 'GET':
+                GetCommand(lines_in_response, version, connectionSocket, url, IPAddr, serverPort)
+            case 'HEAD':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case 'POST':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case 'PUT':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case 'DELETE':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case 'CONNECT':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case 'OPTIONS':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case 'TRACE':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case 'PATCH':
+                NotImplmented(version, connectionSocket, IPAddr)
+            case _: # Should be bad request because not a normal command
+                BadRequest(version, connectionSocket, IPAddr)
+        connectionSocket.close()
+
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    
+    # sentence = connectionSocket.recv(1024).decode()
+    # capitalizedSentence = sentence.upper()
+    # connectionSocket.send(capitalizedSentence.encode())
+
+    # client acquire the lock
+    print('address:', addr[0], ':', addr[1])
+
+    # open a new thread
+    start_new_thread(mutil_thread,(connectionSocket,))
+    # connectionSocket.close()
+
+serverSocket.close()
---
+++ toHanbIn/proxyserver/HTTPfunctions.py
@@ -0,0 +1,140 @@
+from socket import *
+from time import gmtime, strftime, strptime
+import os
+from calendar import timegm
+
+
+def makeHTTPresponse(statusLine, data, url, IPAddr):
+
+    response_lines = []
+    response_lines.append(statusLine)
+    response_lines.append('Date: ' + strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime()))
+    if url != None:
+        response_lines.append('Last-Modified: ' + strftime("%a, %d %b %Y %H:%M:%S GMT", gmtime(os.path.getmtime(url))))
+    response_lines.append('Server: ' + IPAddr)
+    if data != None:
+        response_lines.append('Accept-Ranges: bytes')
+        response_lines.append('Content-Length: ' + str(len(data.encode('utf-8'))))
+    response_lines.append('')
+    if data != None:
+        response_lines.append(data)
+    to_send = ('\r\n').join(response_lines)
+    return to_send
+
+def GetCommand(lines_in_response, version, connectionSocket, url, IPAddr, serverPort):
+    
+    # get the url
+    if url.find('https://') != -1:
+        start = url.find('https://') + len('https://')
+        url = url[1 + url.find('/',start):]
+    elif url.find('http://') != -1:
+        start = url.find('http://') + len('http://')
+        url = url[1 + url.find('/',start + 1):]
+    else:
+        url = url[1:]
+
+    host = 'error'
+    for line in lines_in_response:
+        #print('test', line)
+        if line[:6] == 'Host: ':
+            host = line[6:]
+            if(host != (IPAddr + ':'+ str(serverPort))):
+                #proxy server stuff
+                BadRequest(version, connectionSocket, IPAddr)
+                return True
+        elif line[:19] == 'If-Modified-Since: ':
+            t_string = line[19:(19+25)]
+            #print(t_string)
+            if os.path.getmtime(url) < timegm(strptime(t_string, '%a, %d %b %Y %H:%M:%S')):
+                NotModified(version, connectionSocket, url, IPAddr)
+                return True
+    
+    # if the host line is not there send bad request
+    if host == 'error':
+        BadRequest(version, connectionSocket, IPAddr)
+    
+    # try to open the file
+    try:
+        f = open(url, 'r', encoding='utf-8')
+        try:
+            data = f.read()
+        except Exception as e:
+            print(e)
+            f.close()
+            return True
+    except Exception as e:
+        print(e)
+        NotFound(version, connectionSocket, IPAddr)
+        return True
+    f.close()
+    
+    
+    to_send = makeHTTPresponse(version + ' 200 OK', data, url, IPAddr)
+
+    print(to_send)
+    connectionSocket.send(to_send.encode())
+    return False
+    
+
+
+def NotImplmented(version, connectionSocket, IPAddr):
+    try:
+        f = open('501.html', 'r', encoding='utf-8')
+        try:
+            data = f.read()
+        except Exception as e:
+            print(e)
+            f.close()
+            return True
+    except Exception as e:
+        print(e)
+        return True
+    f.close()
+    to_send = makeHTTPresponse(version + ' 501 Not Implemented', data, '501.html', IPAddr)
+    print(to_send)
+    connectionSocket.send(to_send.encode())
+
+def BadRequest(version, connectionSocket, IPAddr):
+    try:
+        f = open('400.html', 'r', encoding='utf-8')
+        try:
+            data = f.read()
+        except Exception as e:
+            print(e)
+            f.close()
+            return True
+    except Exception as e:
+        print(e)
+        return True
+    f.close()
+
+    to_send = makeHTTPresponse(version + ' 400 Bad request', data, '400.html', IPAddr)
+    print(to_send)
+    connectionSocket.send(to_send.encode())
+
+def NotFound(version, connectionSocket, IPAddr):
+
+    try:
+        f = open('404.html', 'r', encoding='utf-8')
+        try:
+            data = f.read()
+        except Exception as e:
+            print(e)
+            f.close()
+            return True
+    except Exception as e:
+        print(e)
+        return True
+    f.close()
+
+    to_send = makeHTTPresponse(version + ' 404 Not Found', data, '404.html', IPAddr)
+    print(to_send)
+    connectionSocket.send(to_send.encode())
+
+
+
+def NotModified(version, connectionSocket, url, IPAddr):
+    
+    to_send = makeHTTPresponse(version + ' 304 Not modified', None, url, IPAddr)
+    print(to_send)
+    connectionSocket.send(to_send.encode())
